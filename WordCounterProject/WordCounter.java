4545e895e72622414c7a4fc1c0d97a673f868d0b
Aren Oganesyan <arem.h.y@tumo.org>
2024-08-03 19:35:03 +0400

    Update, to work simultaneously with the written text. Added small icon.

--- WordCounterProject/WordCounter.java
+++ WordCounterProject/WordCounter.java
@@ -1,52 +1,73 @@
 package WordCounterProject;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 
 public class WordCounter extends JFrame {
 
     private JTextArea textArea;
-    private JButton countCharactersButton;
-    private JLabel resultLabel;
+    private JLabel wordCountLabel;
+    private JLabel charCountLabel;
 
     public WordCounter() {
         createView();
 
-        setTitle("Text Counter");
+        setTitle("Word Counter");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400, 300);
         setLocationRelativeTo(null);
         setResizable(false);
+        setIcon();
     }
 
     private void createView() {
         JPanel panel = new JPanel();
         getContentPane().add(panel);
-
         panel.setLayout(new BorderLayout());
 
         textArea = new JTextArea();
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateCounts();
+            }
+        });
+        JPanel resultPanel = new JPanel();
+        panel.add(resultPanel, BorderLayout.SOUTH);
+
         JScrollPane scrollPane = new JScrollPane(textArea);
         panel.add(scrollPane, BorderLayout.CENTER);
 
-        JPanel buttonPanel = new JPanel();
-        panel.add(buttonPanel, BorderLayout.SOUTH);
+        wordCountLabel = new JLabel("Word count: 0");
+        resultPanel.add(wordCountLabel);
 
-        JButton countWordsButton = new JButton("Count Words");
-        countWordsButton.addActionListener(e -> {
-            int wordCount = WordCounterConsole.wordCount(textArea.getText());
-            resultLabel.setText("Word count: " + wordCount);
-        });
-        buttonPanel.add(countWordsButton);
+        charCountLabel = new JLabel("Character count: 0");
+        resultPanel.add(charCountLabel);
+    }
 
-        countCharactersButton = new JButton("Count Characters");
-        countCharactersButton.addActionListener(e -> {
-            int charCount = WordCounterConsole.characterCount(textArea.getText());
-            resultLabel.setText("Character count: " + charCount);
-        });
-        buttonPanel.add(countCharactersButton);
+    private void updateCounts() {
+        String text = textArea.getText();
+        int wordCount = WordCounterConsole.wordCount(text);
+        int charCount = WordCounterConsole.characterCount(text);
+        wordCountLabel.setText("Word count: " + wordCount);
+        charCountLabel.setText("Character count: " + charCount);
+    }
 
-        resultLabel = new JLabel(" ");
-        panel.add(resultLabel, BorderLayout.NORTH);
+    private void setIcon() {
+        ImageIcon icon = new ImageIcon(getClass().getResource("189816.jpg"));
+        Image image = icon.getImage();
+        setIconImage(image);
     }
 }
